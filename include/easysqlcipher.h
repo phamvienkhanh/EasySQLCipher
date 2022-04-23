09b2d456b1fc21a9c1e13bb9d3a8abf38895b106
keithpham <keithpham>
2022-04-23 23:51:01 +0700

    init project

---
+++ include/easysqlcipher.h
@@ -0,0 +1,10 @@
+#ifndef EASYSQLCIPHER_H
+#define EASYSQLCIPHER_H
+
+class EasySQLCipher
+{
+public:
+    EasySQLCipher();
+};
+
+#endif // EASYSQLCIPHER_H
---
+++ source/easysqlcipher.cpp
@@ -0,0 +1,5 @@
+#include "easysqlcipher.h"
+
+EasySQLCipher::EasySQLCipher()
+{
+}
